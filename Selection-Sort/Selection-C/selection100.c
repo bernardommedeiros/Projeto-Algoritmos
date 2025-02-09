5b14d67b43933ab0ac05e3e4c670fba02a5924fb
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-02-09 15:03:03 -0300

    Update selection100.c

---
+++ Selection-Sort/Selection-C/selection100.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void selectionSort(int arr[], int n) {
+    int i, j, min_idx;
+
+    // move o limite da sublista não classificada
+    for (i = 0; i < n-1; i++) {
+        // Encontre o menor elemento na sublista não classificada
+        min_idx = i;
+        for (j = i+1; j < n; j++) {
+            // Verifica se o elemento arr[j] é menor que o elemento no índice min_idx
+            if (arr[j] < arr[min_idx]) {
+                // Atualiza min_idx para o índice do novo menor elemento
+                min_idx = j;
+            }
+        }
+
+        // Troca o menor elemento encontrado com o primeiro elemento da sublista não classificada
+        int temp = arr[min_idx];
+        arr[min_idx] = arr[i];
+        arr[i] = temp;
+    }
+}
+
+
+int main() {
+    srand(time(0));
+
+    int numerosAleatorios[100];
+
+    for (int i = 0; i < 100; i++) {
+        numerosAleatorios[i] = rand() % 101;
+    }
+
+    int n = sizeof(numerosAleatorios) / sizeof(numerosAleatorios[0]);
+
+    clock_t inicio = clock();
+
+    // ordenar o array
+    selectionSort(numerosAleatorios, n);
+
+    clock_t fim = clock();
+
+    double tempo_execucao = ((double)(fim - inicio)) / CLOCKS_PER_SEC;
+
+    printf("Tempo de execução do Selection Sort: %f segundos\n", tempo_execucao);
+
+    return 0;
+}
