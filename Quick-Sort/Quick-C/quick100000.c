8906b8817a42371f15a862ccf8d900c0fc8e8cc6
Bernardo de Moura Medeiros <161090624+bernardommedeiros@users.noreply.github.com>
2025-03-10 17:40:36 +0000

    quick sort

---
+++ Quick-Sort/Quick-C/quick100000.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void quick_sort(int arr[], int n) {
+    if (n <= 1) {
+        return;
+    }
+
+    int pivo = arr[0];
+    int menores[n], maiores[n];
+    int menores_n = 0, maiores_n = 0;
+
+    for (int i = 1; i < n; i++) {
+        if (arr[i] <= pivo) {
+            menores[menores_n++] = arr[i];
+        } else {
+            maiores[maiores_n++] = arr[i];
+        }
+    }
+
+    quick_sort(menores, menores_n);
+    quick_sort(maiores, maiores_n);
+
+    for (int i = 0; i < menores_n; i++) {
+        arr[i] = menores[i];
+    }
+    arr[menores_n] = pivo;
+    for (int i = 0; i < maiores_n; i++) {
+        arr[menores_n + 1 + i] = maiores[i];
+    }
+}
+
+void generate_random_array(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        arr[i] = rand() % 1000000; 
+    }
+}
+
+int main() {
+    int arr[100000];
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    generate_random_array(arr, n);
+
+    clock_t start = clock();
+    quick_sort(arr, n);
+    clock_t end = clock();
+
+    double time_taken = ((double)(end - start)) / CLOCKS_PER_SEC;
+    printf("Tempo de execução: %f segundos\n", time_taken);
+
+    return 0;
+}
---
+++ Quick-Sort/Quick-Python/quick1000000.py
@@ -0,0 +1,33 @@
+import time
+import random
+
+def quick_sort(arr):
+    if len(arr) <= 1:
+        return arr
+    pivo = arr[0]
+    menores = []
+    maiores = []
+
+    for i in range(1, len(arr)):  
+        if arr[i] <= pivo:
+            menores.append(arr[i])
+        else:
+            maiores.append(arr[i])
+
+    return quick_sort(menores) + [pivo] + quick_sort(maiores)
+
+def quick_time():
+    # gera um array de 100 elementos aleatórios
+    arr = [random.randint(0, 10000000) for i in range(1000000)]
+
+
+    start_time = time.time()
+    quick_sort(arr)
+    end_time = time.time()
+
+    # Calcula o tempo de execução
+    execution_time = end_time - start_time
+    print(f"Tempo de execução: {execution_time:.6f} segundos")
+
+# Executa a função para medir o tempo
+quick_time()
